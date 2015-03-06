0778f942d8803702175af876f9838a053e3554d9
Yanzheng Li <yli@hushmail.me>
2015-03-05 21:18:57 -0800

    [COREVM-174] Clean up bootstrap_tests.py (Closes #104)

--- python/bootstrap_tests.py
+++ python/bootstrap_tests.py
@@ -1,23 +1,25 @@
-# The MIT License (MIT)
+"""
+The MIT License (MIT)
 
-# Copyright (c) 2015 Yanzheng Li
+Copyright (c) 2015 Yanzheng Li
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of
-# this software and associated documentation files (the "Software"), to deal in
-# the Software without restriction, including without limitation the rights to
-# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-# the Software, and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
 
 """
 Bootstrap Python tests.
@@ -37,6 +39,7 @@ PYTHON_SOURCE_DIR = './python/src/'
 PYTHON_COMPILER = './python/python_compiler.py'
 INFO_FILE = './info.json'
 COREVM = './coreVM'
+BYTECODE_EXTENSION = '.core'
 
 
 class bcolors:
@@ -50,35 +53,45 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+def input_to_output_path(path):
+    return path + BYTECODE_EXTENSION
+
+
+def compiler_cmdl_args(path):
+    return [
+        PYTHON,
+        PYTHON_COMPILER,
+        '--input',
+        path,
+        '--info-file',
+        INFO_FILE,
+        '--output',
+        input_to_output_path(path)
+    ]
+
+
+def corevm_cmdl_args(path):
+    return [COREVM, '--input', input_to_output_path(path)]
+
+
 def main():
     inputs = glob.glob(PYTHON_SOURCE_DIR + '*.py')
 
-    print 'Bootstraping tests...'
+    print 'Bootstrapping Python tests...'
     print 'Testing using the following %d input file(s):' % len(inputs)
     for path in inputs:
         print path
 
+    # Bring blank line.
     print
 
     for path in inputs:
         info = path
 
-        output_path = path + '.core'
-        retcode = subprocess.call(
-            [
-                PYTHON,
-                PYTHON_COMPILER,
-                '--input',
-                path,
-                '--info-file',
-                INFO_FILE,
-                '--output',
-                output_path
-            ]
-        )
+        retcode = subprocess.call(compiler_cmdl_args(path))
 
         if retcode == 0:
-            retcode = subprocess.call([COREVM, '--input', output_path])
+            retcode = subprocess.call(corevm_cmdl_args(path))
 
             if retcode == 0:
                 info += (bcolors.OKGREEN + ' [SUCCESS]' + bcolors.ENDC)
