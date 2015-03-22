03701b7040a300943d3e4e3944c079d9d191e369
Yanzheng Li <yli@hushmail.me>
2015-03-22 12:54:40 -0700

    [COREVM-195] Show call stack trace on failures and crashes (Closes #121)

--- src/frontend/runner.cc
+++ src/frontend/runner.cc
@@ -30,11 +30,26 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "../../include/runtime/process.h"
 #include "../../include/runtime/process_runner.h"
 
+#include <sneaker/utility/stack_trace.h>
+
 #include <cerrno>
 #include <cstring>
 #include <iostream>
 #include <string>
 
+
+// -----------------------------------------------------------------------------
+
+const unsigned int DEFAULT_STACK_LEVEL = 5;
+
+// -----------------------------------------------------------------------------
+
+static void
+print_stack_trace()
+{
+  // TODO: [SNEAKER-86] Print stack trace not working well
+  sneaker::utility::stack_trace::print_stack_trace(std::cerr, DEFAULT_STACK_LEVEL);
+}
 
 // -----------------------------------------------------------------------------
 
@@ -74,6 +89,7 @@ corevm::frontend::runner::run() const noexcept
     if (!res)
     {
       std::cerr << "Run failed: " << strerror(errno) << std::endl;
+      print_stack_trace();
       return -1;
     }
   }
@@ -81,18 +97,24 @@ corevm::frontend::runner::run() const noexcept
   {
     std::cerr << "Runtime error: " << ex.what() << std::endl;
     std::cerr << "Abort" << std::endl;
+    print_stack_trace();
+
     return -1;
   }
   catch (const std::exception& ex)
   {
     std::cerr << "Error: " << ex.what() << std::endl;
     std::cerr << "Abort" << std::endl;
+    print_stack_trace();
+
     return -1;
   }
   catch (...)
   {
     std::cerr << "Unknown error" << std::endl;
     std::cerr << "Abort" << std::endl;
+    print_stack_trace();
+
     return -1;
   }
 
