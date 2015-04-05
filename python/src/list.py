c9d76a617aba78334376f41b05bee1773ef2a247
Yanzheng Li <yli@hushmail.me>
2015-04-05 15:29:39 -0700

    [COREVM-220] Fix list.__str__ (Closes #143)

--- python/src/list.py
+++ python/src/list.py
@@ -25,11 +25,29 @@ class list(object):
         ### END VECTOR ###
         """
 
+    def __len__(self):
+        """
+        ### BEGIN VECTOR ###
+        [ldobj, self, 0]
+        [gethndl, 0, 0]
+        [arylen, 0, 0]
+        [new, 0, 0]
+        [sethndl, 0, 0]
+        [stobj, res_, 0]
+        ### END VECTOR ###
+        """
+        return __call(int, res_)
+
     def __str__(self):
+        size = __call(self.__len__)
+        top_index = __call(size.__sub__, 1)
+        index = __call(int, 0)
         res = __call(str, '')
         __call(res.__add__, __call(str, '['))
         for item in self:
             __call(res.__add__, __call(item.__str__)) # res += str(item)
-            __call(res.__add__, __call(str, ', '))
+            if __call(index.__lt__, top_index):
+                __call(res.__add__, __call(str, ', '))
+            index = __call(index.__add__, 1)
         __call(res.__add__, __call(str, ']'))
         return res
--- python/tests/list.py
+++ python/tests/list.py
@@ -1,1 +1,4 @@
+print list([])
+print list([1])
 print list([1, 2])
+print list([1, 2, list([3,4,5])])
